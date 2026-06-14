5bd1510d38a0c9d0bcaddadd2d1d0c3397d491f5
krishnayan kashyap pathak <krishnayankashyap@gmail.com>
2026-06-15 03:01:20 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // This tells Tailwind to look for classNames inside App.tsx and your entire src folder!
+  // This tells Tailwind to look for classNames inside App.tsx and entire src folder!
   content: [
     "./App.{js,jsx,ts,tsx}", 
     "./src/**/*.{js,jsx,ts,tsx}"
